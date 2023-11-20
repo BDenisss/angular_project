3b69e7699f53aabc53166a528d387633cef367a7
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2023-11-20 22:15:43 +0100

    :hammer: add module.ts and enhance route.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+
+        // autres composants déclarés ici...
+    ],
+    imports: [
+        BrowserModule,
+        // autres modules importés ici...
+    ],
+    providers: [
+        // services fournis ici...
+    ],
+    bootstrap: []
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'projects', component: ProjectsListComponent },
+    { path: '', redirectTo: '/projects', pathMatch: 'full' }
+];
